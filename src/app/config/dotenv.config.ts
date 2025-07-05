e2e33947379fbc8e0720409031023959bd07637e
= <=>
2025-07-05 11:26:58 -0300

    refactor: melhorias gerais no código

--- src/app/config/dotenv.config.ts
+++ src/app/config/dotenv.config.ts
@@ -1,13 +1,10 @@
 // import 'dotenv/config';
 import * as dotenv from "dotenv";
 import path, { dirname } from "node:path";
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url); // Resolve o caminho completo do arquivo atual
 const __dirname = dirname(__filename); // Resolve o diretório onde o arquivo está localizado
 
-
-export const loadNodeEnvironment = (): dotenv.DotenvConfigOutput => dotenv.config({
-  path: `${__dirname}/../../../${process.env.NODE_ENV === 'production' ? '.env' : '.env.development'}`
-});
-
+export const loadNodeEnvironment = (): dotenv.DotenvConfigOutput =>
+  dotenv.config({ path: `${__dirname}/../../../.env` });
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { loadNodeEnvironment } from "./app/config/dotenv.config.js";
 
 loadNodeEnvironment();
 
-app.listen(process.env.PORT || process.env.STATIC_PORT);
+app.listen(process.env.PORT || 3000, () => {
+  if (process.env.NODE_ENV !== "production")
+    console.log(`Running on ${process.env.URL}`);
+});
 
 // Fechar o pool quando o processo for interrompido (SIGINT)
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await closePool(); // Fecha o pool de forma assíncrona
   process.exit(0); // Encerra o processo após o fechamento
 });
-
-  
